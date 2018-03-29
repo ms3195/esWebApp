4258730e49097fc497100e3b94fec3b633abbd98
Michael Shao <33072963+ms3195@users.noreply.github.com>
2018-03-29 16:10:14 -0400

    added click function
    slowly getting there. can differentiate between spacebar syntax and actual JS. understand how templates and helpers work.

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 if(Meteor.isClient){
   // this code only runs on the client
+  Template.leaderboard.helpers({
+    //helper functions go here
+    'player': function(){
+      return PlayersList.find();
+    },
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("you clicked a .player element");
+    },
+  });
+
 }
 
 if (Meteor.isServer){
