4931cf3cdf8702dfa83802dce74434dd2d5ee006
Michael Shao <33072963+ms3195@users.noreply.github.com>
2018-04-02 16:11:39 -0400

    roster begins here
    finished up to "Forms" chapter in tutorial. changed all player, Player, and Leaderboards to employee, Employee, and Roster. works! next step is to add several fields to make sure it retains functionality.

--- main.js
+++ main.js
@@ -1,17 +1,61 @@
 if(Meteor.isClient){
   // this code only runs on the client
-  Template.leaderboard.helpers({
+  Template.roster.helpers({
     //helper functions go here
-    'player': function(){
-      return PlayersList.find();
+    'employee': function(){
+      return EmployeesList.find({}, {sort: {score: -1, name: 1} });
+    },
+    'selectedClass': function(){
+      var employeeId = this._id;
+      var selectedEmployee = Session.get('selectedEmployee');
+      if (employeeId == selectedEmployee){
+        return "selected";
+      }
+    },
+    'selectedEmployee': function(){
+      var selectedEmployee = Session.get('selectedEmployee');
+      return EmployeesList.findOne({ _id: selectedEmployee });
     },
   });
 
-  Template.leaderboard.events({
-    'click .player': function(){
-      console.log("you clicked a .player element");
+  Template.roster.events({
+    //part of highlight selected employee, rest is in css
+    'click .employee': function(){
+      var employeeId = this._id;
+      Session.set('selectedEmployee', employeeId);
     },
+    //add 5 points button functionality
+    'click .increment': function(){
+      var selectedEmployee = Session.get('selectedEmployee');
+      EmployeesList.update({ _id: selectedEmployee }, { $inc: {score: 5}});
+    },
+    'click .decrement': function(){
+      var selectedEmployee = Session.get('selectedEmployee');
+      EmployeesList.update({ _id: selectedEmployee }, { $inc: {score: -5}});
+    },
+    'click .remove': function(){
+      var selectedEmployee = Session.get('selectedEmployee');
+      EmployeesList.remove({ _id: selectedEmployee });
+    }
   });
+
+Template.addEmployeeForm.events({
+  'submit form': function(){
+    event.preventDefault();
+    var employeeNameVar = event.target.employeeName.value;
+    var employeeAgeVar = event.target.employeeAge.value;
+    console.log(employeeNameVar);
+    console.log(employeeAgeVar);
+
+    EmployeesList.insert({
+      name: employeeNameVar,
+      age: employeeAgeVar,
+      score: 0
+    });
+    event.target.employeeName.value = "";
+    event.target.employeeAge.value = "";
+  }
+});
 
 }
 
@@ -19,4 +63,4 @@ if (Meteor.isServer){
   // CODE RUNS ON SERVER only
 }
 
-PlayersList = new Mongo.Collection('players');
+EmployeesList = new Mongo.Collection('employees');
