252f4d6abe3a959c1aab5fc34cc82835efe1d9b9
Michael Shao <33072963+ms3195@users.noreply.github.com>
2018-05-30 15:33:35 -0400

    fixed url routes, trying to pull params

---
+++ imports/ui/edit/edit.js
@@ -0,0 +1,3 @@
+console.log("URL IS SUPPOSED TO BE AFTER THIS")
+console.log(FlowRouter.current().params)
+console.log("URL IS SUPPOSED TO BE BEFORE THIS")
--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -1,15 +1,16 @@
 Template.main.events({
   'click #homeButton': function () {
-    Router.go('/');
+    FlowRouter.go('/');
     console.log("navigating to home");
   },
+
   'click #rosterButton': function () {
-    Router.go('/roster');
+    FlowRouter.go('/roster');
     console.log("navigating to roster");
 
   },
   'click #metricsButton': function () {
-    Router.go('/metrics');
+    FlowRouter.go('/metrics');
     console.log("navigating to metrics");
 
   },
