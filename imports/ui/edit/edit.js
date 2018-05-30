330a50bd9e4026c9710fd1cdb2260739e2e9511a
Michael Shao <33072963+ms3195@users.noreply.github.com>
2018-05-30 15:40:12 -0400

    fixed param pull
    had to stick the console.log(pull param) inside an Template.(templatename).onRendered function. FlowRouter renders the template itself, not the whole file.

--- imports/ui/edit/edit.js
+++ imports/ui/edit/edit.js
@@ -1,3 +1,5 @@
-console.log("URL IS SUPPOSED TO BE AFTER THIS")
-console.log(FlowRouter.current().params)
-console.log("URL IS SUPPOSED TO BE BEFORE THIS")
+Template.editEmployee.onRendered(function() {
+  console.log("URL IS SUPPOSED TO BE AFTER THIS")
+  console.log(FlowRouter.current().params);
+  console.log("URL IS SUPPOSED TO BE BEFORE THIS")
+});
